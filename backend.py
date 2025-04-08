26385cfd39cebe943972b425a5ab83f6998d9402
MaramAlfahmi <cs.maramalfahmi@gmail.com>
2025-04-08 03:49:58 +0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -32,7 +32,7 @@ DB_CONFIG = {
 
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
-model = "gpt-3.5-turbo"
+model = "gpt-4o-mini"
 
 # VECTOR_DB_DIR = "chromadb"
 # os.makedirs(VECTOR_DB_DIR, exist_ok=True)
