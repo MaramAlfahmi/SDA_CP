bb3f4c80001a5ef04aac51400424692fc0832424
MaramAlfahmi <cs.maramalfahmi@gmail.com>
2025-04-27 15:24:18 +0300

    Update variables.tf

--- Terraform/modules/VMSS /variables.tf
+++ Terraform/modules/VMSS /variables.tf
@@ -1,3 +1,4 @@
+#===================================Reaource Group ==================================
 variable "resource_group_name" {
   description = "Azure resource group name"
   type        = string
@@ -7,3 +8,28 @@ variable "location" {
   description = "Region location"
   type        = string
 }
+
+#=================================== Vnet ==================================
+variable "subnet_id" {
+  description = "Subnet Id in the Vnet"
+  type        = string
+}
+
+variable "backend_pool_id" {
+  description = "the backend pool assocciated wtih ip configuartion in vmss"
+  type        = string
+}
+
+#=================================== VMSS ==================================
+variable "vmss_name" {
+  description = "Then name of the VM Scale Set"
+  type        = string
+}
+
+#=================================== VMSS image ==================================
+variable "source_image_id" {
+  description = "The image you want to use in the VMSS"
+  type        = string
+}
+
+
