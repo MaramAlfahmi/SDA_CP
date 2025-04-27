a033db661c3f3dba10f3233fcc8950633c8721da
MaramAlfahmi <cs.maramalfahmi@gmail.com>
2025-04-27 15:23:34 +0300

    Update outputs.tf

--- Terraform/modules/VMSS /outputs.tf
+++ Terraform/modules/VMSS /outputs.tf
@@ -1,24 +1,4 @@
-output "vmss_id" {  
-  description = "ID of the Virtual Machine Scale Set"  
-  value       = azurerm_virtual_machine_scale_set.example.id  
-}  
-
-output "vmss_public_ip" {  
-  description = "Public IP addresses of the VM instances"  
-  value       = azurerm_public_ip.example.ip_address  
-}  
-
-output "admin_username" {  
-  description = "Admin username for accessing the VM instances"  
-  value       = var.admin_vmss_username  
-}  
-
-output "vmss_instance_count" {  
-  description = "Number of instances in the VMSS"  
-  value       = var.instance_count  
-}  
-
-output "resource_group_name" {  
-  description = "Resource Group Name"  
-  value       = azurerm_resource_group.main.name  
-}  
+#================================ principal_id =========================================
+output "vmss_managed_identity" {
+  value = azurerm_linux_virtual_machine_scale_set.vmss.identity.0.principal_id
+}
