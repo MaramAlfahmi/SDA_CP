c56ddccca1f7e8e16e5d701ce6fdd9d00cc96f03
MaramAlfahmi <cs.maramalfahmi@gmail.com>
2025-04-27 16:29:42 +0300

    Delete Terraform/modules/VMSS /outputs.tf

--- Terraform/modules/VMSS /outputs.tf
+++
@@ -1,4 +0,0 @@
-#================================ principal_id =========================================
-output "vmss_managed_identity" {
-  value = azurerm_linux_virtual_machine_scale_set.vmss.identity.0.principal_id
-}
